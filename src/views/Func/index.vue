84931e116cec5a3ef5079dc0e973983ea6079a7e
mickey <hitmickeyq@gmail.com>
2026-02-12 00:43:11 +0800

    0.3.0，修改views/func/index.vue

--- src/views/Func/index.vue
+++ src/views/Func/index.vue
@@ -65,10 +65,10 @@ onBeforeUnmount(() => {
   height: auto;
 
   // --- 修改 2: 使用 margin-top 让模块整体下移 (数值可根据需要调整) ---
-  margin-top: 50px;
+  margin-top: 12vh;
 
   // --- 修改 3: 压缩模块底部的外边距，缩短与下方服务器模块的间隔 ---
-  margin-bottom: 0px;
+  margin-bottom: -40px;
 
   display: flex;
   flex-direction: row;
